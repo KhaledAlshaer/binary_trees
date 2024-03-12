f278a30d918e4b484ef61e5cb1a8e24c92571ce0
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-03-12 07:20:24 -0700

    1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+"binary_trees.h"
+
+/**
+ *binary_tree_insert_left - this is the function
+ *@parent: this is a vraiable
+ *@value: this is a vraiable
+ *Return: return 0
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		node->left->parent = node;
+	}
+
+	parent->left = node;
+
+	return (node);
+}
