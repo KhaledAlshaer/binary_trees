d5d54e845d2ed87e092c72522fd76811f219f8cb
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-03-15 09:00:51 -0700

    4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_leaf - this is the function
+ *@node: this is a vraiable
+ *Return: return 0
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left || node->right)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
