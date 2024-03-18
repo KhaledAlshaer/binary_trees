60231806b6eaf56dbfe41b1d2ed46abd9ce24058
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-03-18 08:14:33 -0700

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,7 +14,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree->parent != NULL)
 	{
-		depth = binary_tree_depth(tree->parent) + 1 ;
+		depth = binary_tree_depth(tree->parent) + 1;
 	}
 
 	return (depth);
