0d742ca7f85d8d4b71873e3d68461233511019cc
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-04-06 02:33:34 -0700

    17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->n < node->parent->n)
 		return (node->parent->right);
 	else if (node->n > node->parent->n)
-		return (node->parent->left)
+		return (node->parent->left);
 	else
 		return (NULL);
 }
